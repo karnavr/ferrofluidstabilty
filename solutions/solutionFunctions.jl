66811d01cc0c10183bf2781b2e89d1c38678f726
karnavr <kraval3@uwo.ca>
2024-08-11 16:23:52 -0400

    clean up

--- solutions/solutionFunctions.jl
+++ solutions/solutionFunctions.jl
@@ -342,8 +342,8 @@ function bifurcation(initial_guess::Matrix{Float64}, a1Vals::Vector{Float64}, br
 		JSON.print(f, meta)
 	end
 
-
 	return solutions, iterations
+
 end
 
 
@@ -460,4 +460,45 @@ function plotting(solution_file::String)
 	# flag_plot = plot((meta["a1Vals"])[1:end-1], meta["flags"], xlabel=L"a_1", ylabel="Flag", legend=false, seriestype = :line, marker = :dot, markersize = 2)
 
 	return profile_plot, branch_plot, coeff_plot, convergence_plot, error_plot
+end
+
+function recompute_solutions(file_path::String)
+
+	# get names of the files in the results folder
+	files = readdir(file_path)
+
+	# loop over the files
+	for file in files
+		
+		# get the parameters from the file name 
+		# (note that the second parameter is the tolerance, which has a decimal point in it, so we need to add the "1" to the start of the string to parse it as a float)
+		parameters = split(file, ".")
+		N = parse(Int, parameters[1])
+		tol = parse(Float64, "1." * parameters[3])
+		branchN = parse(Int, parameters[4])
+		solver = parameters[5]
+
+		# print each for the first 10 files 
+		if file == files[1:10]
+			println("N: ", N)
+			println("tol: ", tol)
+			println("branchN: ", branchN)
+			println("solver: ", solver)
+		end
+
+		# initialize everything
+		a1Vals = collect(range(0.001, 0.33, branchN + 1))		  # a1 values
+
+		constants = Constants(N,π,1.5,0.1) 					      # constants  
+		k1 = 1*π/constants.L			   					      # wave number
+		cInitial = c0(k1, constants);	   					      # wave speed 
+
+		initial_guess = (1e-16).*ones(branchN+1, constants.N + 2) # initial guess 
+		initial_guess[1,1:4] = [cInitial, 1.0, a1Vals[1], 1e-16]
+
+		# solve 
+		bifurcation(initial_guess, a1Vals, branchN, constants, tol = tol, solver = Symbol(solver), max_iter = 40000)
+
+	end
+
 end
